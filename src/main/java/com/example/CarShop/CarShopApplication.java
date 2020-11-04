22a1ec3d8f69c8d33de869bf0437bd7be8e97f93
MAUMA <maurizio.macri@hm.com>
2020-11-04 14:54:19 +0100

    initial commit

---
+++ src/main/java/com/example/CarShop/CarShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShopApplication.class, args);
+	}
+
+}
