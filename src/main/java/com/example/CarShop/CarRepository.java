f2a936d7215c621b5f447553c3e7c85c332e2ff5
HM\FRBUL <fredrik.bullsimonsen@hm.com>
2020-11-05 15:18:02 +0100

    Added buttons on cardetails and added buyCar page.

--- src/main/java/com/example/CarShop/CarRepository.java
+++ src/main/java/com/example/CarShop/CarRepository.java
@@ -9,7 +9,7 @@ public class CarRepository {
         List<Car> carList = new ArrayList<>();
         public CarRepository()
         {
-            addCar(new Car("SAAB","9-5", 10000, 1999, 7000, "Soltak, automat", ""));
+            addCar(new Car("SAAB","9-5", 10000, 1999, 7000, "Soltak, automat", "http://lh4.ggpht.com/-C41mB8UNUAs/UPCxFdbGQWI/AAAAAAAAlcg/TpktfNFbgDc/s1600/2004-saab-9-5-sedan-00011.jpg"));
             addCar(new Car("Volvo", "V70", 8000, 2012, 95000, "En ägare", ""));
             addCar(new Car("Koeningsegg", "1", 2000, 2018, 700000, "UNIK!", ""));
             addCar(new Car("BMW", "320", 10000, 2001, 80000, "Rostig", ""));
