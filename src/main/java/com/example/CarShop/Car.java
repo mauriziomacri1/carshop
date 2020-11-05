26b2b325bb123733e1e3648cba16870577531064
HM\FRBUL <fredrik.bullsimonsen@hm.com>
2020-11-05 08:59:36 +0100

    Added fields in Car class

--- src/main/java/com/example/CarShop/Car.java
+++ src/main/java/com/example/CarShop/Car.java
@@ -2,19 +2,41 @@ package com.example.CarShop;
 
 public class Car {
     private String brand;
+    private String model;
     private int miles;
     private int year;
     private int price;
+    private String description;
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        model = model;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 
     public void setBrand(String brand) {
         this.brand = brand;
     }
 
-    public Car(String brand, int miles, int year, int price) {
+    public Car(String brand, String model, int miles, int year, int price, String description) {
         this.brand = brand;
+        this.model = model;
         this.miles = miles;
         this.year = year;
         this.price = price;
+        this.description = description;
     }
 
     public String getBrand() { return brand;    }
--- src/main/java/com/example/CarShop/CarRepository.java
+++ src/main/java/com/example/CarShop/CarRepository.java
@@ -7,13 +7,13 @@ import java.util.List;
 public class CarRepository {
 
         List<Car> carList = new ArrayList<>();
-        public CarRepository()
+        public CarRepository()  
         {
-            addCar(new Car("SAAB", 10000, 1999, 7000));
-            addCar(new Car("Volvo", 8000, 2012, 95000));
-            addCar(new Car("Koeningsegg", 2000, 2018, 700000));
-            addCar(new Car("BMW", 10000, 2001, 80000));
-            addCar(new Car("Audi", 10000, 1999, 7000));
+            addCar(new Car("SAAB","9-5", 10000, 1999, 7000, "Soltak, automat"));
+            addCar(new Car("Volvo", "V70", 8000, 2012, 95000, "En ägare"));
+            addCar(new Car("Koeningsegg", "1", 2000, 2018, 700000, "UNIK!"));
+            addCar(new Car("BMW", "320", 10000, 2001, 80000, "Rostig"));
+            addCar(new Car("Audi", "A6", 10000, 1999, 7000, "Fint skick"));
         }
         public void addCar(Car car) {
             carList.add(car);
